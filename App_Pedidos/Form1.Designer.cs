61e049a3aa04efe0b2f6b7d526efd56d7ed6bf69
Rosana <rosanare.cr@gmail.com>
2025-07-16 17:32:22 -0600

    Revert "cambio diseño login"
    This reverts commit dd172ec4faff400305b88acba1fe0aa232d247cc.

--- App_Pedidos/Form1.Designer.cs
+++ App_Pedidos/Form1.Designer.cs
@@ -34,17 +34,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.Logo = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.OrangeRed;
-            this.button1.Location = new System.Drawing.Point(104, 385);
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(116, 340);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(255, 48);
             this.button1.TabIndex = 0;
@@ -53,14 +50,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 294);
+            this.textBox1.Location = new System.Drawing.Point(116, 285);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(255, 26);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(104, 228);
+            this.textBox2.Location = new System.Drawing.Point(116, 222);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(255, 26);
             this.textBox2.TabIndex = 2;
@@ -68,13 +65,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(199, 60);
+            this.label1.Location = new System.Drawing.Point(142, 97);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 47);
+            this.label1.Size = new System.Drawing.Size(255, 50);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Hola!";
+            this.label1.Text = "Best Burgers";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -82,7 +79,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(100, 269);
+            this.label2.Location = new System.Drawing.Point(112, 260);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(113, 22);
             this.label2.TabIndex = 4;
@@ -93,42 +90,19 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(100, 203);
+            this.label3.Location = new System.Drawing.Point(112, 197);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(72, 22);
             this.label3.TabIndex = 5;
             this.label3.Text = "Correo";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // Logo
-            // 
-            this.Logo.Image = global::App_Pedidos.Properties.Resources.Captura_de_pantalla_2025_07_16_164225;
-            this.Logo.Location = new System.Drawing.Point(104, 60);
-            this.Logo.Name = "Logo";
-            this.Logo.Size = new System.Drawing.Size(89, 81);
-            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.Logo.TabIndex = 6;
-            this.Logo.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(201, 107);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(169, 34);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Bienvenido";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(479, 516);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.Logo);
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(507, 543);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -137,7 +111,6 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,8 +124,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox Logo;
-        private System.Windows.Forms.Label label4;
     }
 }
 
