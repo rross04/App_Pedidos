69495b4c6f4623cf0ee62ed253aa65080b5f2cea
fabian <fabogz281@gmail.com>
2025-07-17 18:08:03 -0600

    Create DB connection file

---
+++ App_Pedidos/Database/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace App_Pedidos.Database
+{
+    public class Connection
+    {
+        // Connection string for SQL Server database
+       // private readonly string connectionString =
+        //    "Data Source=localhost;Initial Catalog=Pedidos;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True";
+
+        /*Use one or the other string, depending on whether you have express or normal sqlserver*/
+        
+          private readonly string connectionString =
+         "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Pedidos;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True";
+ 
+
+
+        //Method to get a new SQL connection
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
